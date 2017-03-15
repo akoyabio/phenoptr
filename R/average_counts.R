e14d7c42f93dd85ad44f327c41565d873d8f5271
Kent Johnson <kent.johnson@perkinelmer.com>
2017-03-15 10:53:20 -0400

    More work on count_within
    - Rename it back to count_within to focus on the counts.
    - Allow passing a vector of radii.
    - Don't return the within_count, it double-counts cells.
    - Better docs.

--- R/average_counts.R
+++ R/average_counts.R
@@ -1,32 +1,48 @@
-# Functions to compute average counts
+# Functions to compute counts and average counts
 
-#' Find the number of cells within a radius
+#' Count cells within a radius
 #'
-#' Find the average number of cells of type \code{to} within \code{radius}
-#' of \code{from} cells
-#' within tissue category \code{category}.
+#' Count the number of \code{from} cells having a \code{to} cell within
+#' \code{radius} microns in tissue category \code{category}.
+#' Compute the average number of \code{to} cells
+#' within \code{radius} of \code{from} cells.
+#'
+#' For each \code{from} cell, count the number of \code{to} cells within
+#' \code{radius} microns. Report the number of \code{from} cells containing
+#' at least \emph{one} \code{to} cell within \code{radius} as \code{from_with}.
+#' Report the \emph{average} number of \code{to} cells as \code{within_mean}.
+#'
+#' If \code{category} is specified, all reported values are for cells within
+#' the given tissue category. If \code{category} is NULL, values are reported
+#' for the entire data set.
+#'
+#' \code{radius} may be a vector with multiple values.
+#'
 #' @param csd A data frame with \code{Cell X Position},
 #'        \code{Cell Y Position} and \code{Phenotype} columns,
 #'        such as the result of calling \code{\link{read_cell_seg_data}}.
 #' @param from,to Selection criteria for the
 #' rows and columns. Accepts all formats accepted by \code{\link{select_rows}}.
-#' @param radius The radius to search within.
+#' @param radius The radius or radii to search within.
 #' @param category Optional tissue category to restrict both \code{from} and
 #' \code{to}.
 #' @param dst Optional distance matrix corresponding to \code{csd},
 #'        produced by calling \code{\link{distance_matrix}}.
-#' @return A \code{\link{data_frame}} with four columns:
+#'
+#' @return A \code{\link{data_frame}} with four columns and one row for each
+#'   value in \code{radius}:
 #'   \describe{
-#'    \item{\code{from_count}}{The number of \code{from} cells found.}
-#'    \item{\code{to_count}}{The number of \code{to} cells found.}
-#'    \item{\code{within_count}}{The number of \code{to} cells found within
-#'    \code{radius} microns of a \code{from} cell.}
+#'    \item{\code{radius}}{The value of \code{radius} for this row.}
+#'    \item{\code{from_count}}{The number of \code{from} cells found in \code{csd}.}
+#'    \item{\code{to_count}}{The number of \code{to} cells found in \code{csd}.}
+#'    \item{\code{from_with}}{The number of \code{from} cells with a
+#'    \code{to} cell within \code{radius}.}
 #'    \item{\code{within_mean}}{The average number of \code{to} cells found
-#'    within \code{radius} microns of a \code{from} cell.}
+#'    within \code{radius} microns of each \code{from} cell.}
 #'  }
 #' @export
 #' @family distance functions
-average_count_within = function(csd, from, to, radius, category=NULL, dst=NULL)
+count_within = function(csd, from, to, radius, category=NULL, dst=NULL)
 {
   if (is.null(dst))
     dst = distance_matrix(csd)
@@ -42,18 +58,22 @@ average_count_within = function(csd, from, to, radius, category=NULL, dst=NULL)
 
   dst = subset_distance_matrix(csd, dst, from, to)
   if (prod(dim(dst))>0) {
-    within = apply(dst, 1, function(r) sum(r<=radius))
-    tibble::data_frame(
-      from_count = dim(dst)[1],
-      to_count = dim(dst)[2],
-      within_count = sum(within),
-      within_mean = mean(within)
+    purrr::map_df(radius, function(rad) {
+      within = apply(dst, 1, function(r) sum(r<=rad))
+      tibble::data_frame(
+        radius = rad,
+        from_count = dim(dst)[1], # Number of from cells
+        to_count = dim(dst)[2],   # Number of to cells
+        from_with = sum(within>0), # Number of from cells having a to cell within radius
+        within_mean = mean(within)  # Mean number of to cells within radius of a from cell
+      )}
     )
   } else {
     tibble::data_frame(
+      radius = radius,
       from_count = dim(dst)[1],
       to_count = dim(dst)[2],
-      within_count = 0,
+      from_with = 0,
       within_mean = 0
     )
   }
--- tests/testthat/test_average_counts.R
+++ tests/testthat/test_average_counts.R
@@ -2,37 +2,69 @@
 library(testthat)
 library(dplyr)
 
-check_within = function(within, from, to) {
-  expect_equal(nrow(within), 1)
+check_within = function(within, radius, from, to) {
+  expect_equal(nrow(within), length(radius))
   expect_equal(names(within),
-               c("from_count", "to_count", "within_count", "within_mean"))
+               c("radius", "from_count", "to_count", "from_with", "within_mean"))
   expect_equal(within$from_count, from)
   expect_equal(within$to_count, to)
 }
 
 # Smoke test of average_counts. Mostly checks that it doesn't barf and
 # that the correct cells are selected.
-test_that("average_counts works", {
+test_that("count_within works", {
   csd = sample_cell_seg_data %>% filter(Phenotype != 'other')
   dst = distance_matrix(csd)
 
-  within15 = average_count_within(csd, 'tumor', 'cytotoxic CD8', 15, dst=dst)
-  check_within(within15, 3303, 293)
+  within15 = count_within(csd, 'tumor', 'cytotoxic CD8', 15, dst=dst)
+  check_within(within15, 15, 3303, 293)
 
-  within30 = average_count_within(csd, 'tumor', 'cytotoxic CD8', 30, dst=dst)
-  check_within(within30, 3303, 293)
-  expect_gt(within30$within_count, within15$within_count)
+  within30 = count_within(csd, 'tumor', 'cytotoxic CD8', 30, dst=dst)
+  check_within(within30, 30, 3303, 293)
   expect_gt(within30$within_mean, within15$within_mean)
 
-  within15tumor = average_count_within(csd, 'tumor', 'cytotoxic CD8', 15,
+  within15tumor = count_within(csd, 'tumor', 'cytotoxic CD8', 15,
                                        'tumor', dst=dst)
-  check_within(within15tumor, 3221, 129)
+  check_within(within15tumor, 15, 3221, 129)
 
-  within = average_count_within(csd, 'tumor',
+  within = count_within(csd, 'tumor',
                         c('cytotoxic CD8', 'helper CD4'), 15, 'tumor', dst=dst)
-  check_within(within, 3221, 129+6)
+  check_within(within, 15, 3221, 129+6)
 
-  within = average_count_within(csd,
+  within = count_within(csd,
                         c('cytotoxic CD8', 'helper CD4'), 'tumor',  15, 'tumor', dst=dst)
-  check_within(within, 129+6, 3221)
+  check_within(within, 15, 129+6, 3221)
+})
+
+test_that("count_within works with no data", {
+  csd = sample_cell_seg_data %>% filter(Phenotype != 'other')
+  dst = distance_matrix(csd)
+
+  within = count_within(csd, 'other', 'cytotoxic CD8', 15, dst=dst)
+  check_within(within, 15, 0, 293)
+  expect_equal(within$from_with, 0)
+  expect_equal(within$within_mean, 0)
+
+  within = count_within(csd, 'tumor', 'other', 15, dst=dst)
+  check_within(within, 15, 3303, 0)
+  expect_equal(within$from_with, 0)
+  expect_equal(within$within_mean, 0)
+
+  within = count_within(csd, 'other', 'other', 15, dst=dst)
+  check_within(within, 15, 0, 0)
+  expect_equal(within$from_with, 0)
+  expect_equal(within$within_mean, 0)
+})
+
+test_that("count_within works with multiple radii", {
+  csd = sample_cell_seg_data %>% filter(Phenotype != 'other')
+  dst = distance_matrix(csd)
+
+  within = count_within(csd, 'tumor', 'cytotoxic CD8', c(15, 30), dst=dst)
+  check_within(within, c(15, 30), c(3303, 3303), c(293, 293))
+
+  within = count_within(csd, 'other', 'other', c(15, 30), dst=dst)
+  check_within(within, c(15, 30), c(0, 0), c(0, 0))
+  expect_equal(within$from_with, c(0, 0))
+  expect_equal(within$within_mean, c(0, 0))
 })
