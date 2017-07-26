06ad86195ea3a632ecb2c59e6ebafadcadac7bbf
Kent Johnson <kent.johnson@perkinelmer.com>
2017-07-26 09:40:13 -0400

    Fix the table in the sample_cell_seg_data docs

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #'
 #' A dataset containing inForm cell segmentation data for a single field.
 #' This is the data from
-#' ```
+#' ```{r eval=FALSE}
 #' system.file("extdata", "sample",
 #'                   "Set4_1-6plex_[16142,55840]_cell_seg_data.txt",
 #'                   package = "phenoptr")
@@ -13,16 +13,17 @@
 #' This table shows the Opal stains used, the epitope they were bound to,
 #' and the colors used to show the stain in the composite and phenotype views.
 #'
-#' Stain   | Epitope | Composite | Phenotype
-#' --------|---------|-----------|----------
-#' DAPI    | Nucleus | Blue      | N/A
-#' Opal520 | PDL1    | Red       | N/A
-#' Opal540 | CD8     | Yellow    | Yellow
-#' Opal570 | FoxP3   | Orange    | Orange
-#' Opal620 | CD68    | Magenta   | Magenta
-#' Opal650 | PD1     | Green     | N/A
-#' Opal690 | CK      | Cyan      | Cyan
-#'
+#' \tabular{llll}{
+#'   Stain   \tab Epitope \tab Composite \tab Phenotype \cr
+#'   ----------- \tab ----------- \tab ------------- \tab ------------- \cr
+#'   DAPI    \tab Nucleus \tab Blue    \tab N/A    \cr
+#'   Opal520 \tab PDL1    \tab Red     \tab N/A    \cr
+#'   Opal540 \tab CD8     \tab Yellow  \tab Yellow \cr
+#'   Opal570 \tab FoxP3   \tab Orange  \tab Orange \cr
+#'   Opal620 \tab CD68    \tab Magenta \tab Magenta\cr
+#'   Opal650 \tab PD1     \tab Green   \tab N/A    \cr
+#'   Opal690 \tab CK      \tab Cyan    \tab Cyan
+#' }
 #' The sample cell data includes phenotypes CD8+ (cytotoxic T cell),
 #' CD68+ (macrophage),
 #' FoxP3+ (regulatory T cell), CK+ (tumor), and other.
