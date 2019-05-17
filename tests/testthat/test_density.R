88a6cf3ce8de13d819332bbae820573f2ea47dda
Kent Johnson <kjohnson@akoyabio.com>
2019-05-17 14:44:01 -0400

    Fix calculation of density bands when cell seg data is in microns (ticket #10).
    When inForm writes cell data in microns, it uses the slide origin for the cell location. This has to be subtracted out to get image coordinates.
    
    Note: when read_cell_seg_data converts to microns it does not add the offset so the coordinate origin is at the image top left.

--- tests/testthat/test_density.R
+++ tests/testthat/test_density.R
@@ -3,17 +3,17 @@ context('density')
 library(testthat)
 library(dplyr)
 
-check_density_bands <- function(values) {
+check_density_bands <- function(values, pixels_per_micron) {
   expect_equal(names(values), c("densities", "cells", "distance"))
 
   densities = values$densities
   expect_equal(range(densities$midpoint), c(-112.5, 212.5))
 
   dens_summary = densities %>% group_by(phenotype) %>%
-    summarize(count=sum(count), pixels=sum(area)*4)
+    summarize(count=sum(count), pixels=sum(area)*pixels_per_micron^2)
 
   # Sum of pixels is image size
-  expect_equal(dens_summary$pixels, rep(1868*1400, 3))
+  expect_equal(dens_summary$pixels, rep(1868*1400, 3), tolerance=1)
 
   # Cell counts should match cell_seg_data
   csd_summary = sample_cell_seg_data %>% group_by(Phenotype) %>%
@@ -47,21 +47,24 @@ test_that('density_bands works', {
     list("CD8+", "CD68+", "FoxP3+"),
     positive="Stroma", negative="Tumor")
 
-  check_density_bands(values)
+  check_density_bands(values, pixels_per_micron=2.007538)
 
   # Try with consolidated format
   cell_seg_path = testthat::test_path('test_data', 'consolidated',
                   'Set4_1-6plex_[16142,55840]_cons_cell_seg_data.txt')
 
-  # Get the path to the segmentation map in the sample data
+  # Get the path to the segmentation map & component data for the sample data,
+  # it is the same image as the consolidated data.
   map_path = sub('_cell_seg_data.txt', '_binary_seg_maps.tif',
+                 sample_cell_seg_path())
+  component_path = sub('_cell_seg_data.txt', '_component_data.tif',
                  sample_cell_seg_path())
   values <- density_bands(cell_seg_path,
                           list("CD8+", "CD68+", "FoxP3+"),
                           positive="Stroma", negative="Tumor",
-                          map_path=map_path)
+                          map_path=map_path, component_path=component_path)
 
-  check_density_bands(values)
+  check_density_bands(values, pixels_per_micron=2.007538)
 
 
 })
