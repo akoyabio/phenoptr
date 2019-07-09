69853a728f363a23f6f745149876d50a96be95b6
Kent Johnson <kjohnson@akoyabio.com>
2019-07-09 11:58:48 -0400

    Fix a test. The output file now has an extension.

--- tests/testthat/test_spatial_distribution_report.R
+++ tests/testthat/test_spatial_distribution_report.R
@@ -4,7 +4,7 @@ library(testthat)
 
 test_that('spatial_distribution_report runs', {
   cell_seg_path =
-    file.path('test_data',
+    test_path('test_data',
               'FIHC4__0929309_HP_IM3_2_cell_seg_data.txt') %>%
     normalizePath
 
@@ -12,7 +12,7 @@ test_that('spatial_distribution_report runs', {
     c("B", 'Cytotoxic T'),
     c("B", 'Helper T'))
   colors = c(B='red', 'Helper T'='green', 'Cytotoxic T'="yellow")
-  out_path = tempfile()
+  out_path = tempfile(fileext='.html')
 
   # Note: the warning is from ggplot
   expect_warning(spatial_distribution_report(cell_seg_path, pairs, colors,
@@ -26,7 +26,7 @@ test_that('spatial_distribution_report runs', {
 test_that('spatial_distribution_report works with consolidated data', {
   # Hack some test data to avoid keeping more copies of stuff
   raw_path =
-    file.path('test_data', 'consolidated',
+    test_path('test_data', 'consolidated',
               'FIHC4_consolidated_merge_cell_seg_data.txt') %>%
     normalizePath
 
@@ -46,7 +46,7 @@ test_that('spatial_distribution_report works with consolidated data', {
     c("B+", 'Cytotoxic_T+'),
     c("B+", 'Helper_T+'))
   colors = c('B+'='red', 'Helper_T+'='green', 'Cytotoxic_T+'="yellow")
-  out_path = tempfile()
+  out_path = tempfile(fileext='.html')
 
   # Note: the warning is from ggplot
   expect_warning(spatial_distribution_report(cell_seg_path, pairs, colors,
