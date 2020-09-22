a4138dba9819ec15d754d686d82fb8256ebbf920
Kent Johnson <kjohnson@akoyabio.com>
2020-09-22 12:04:27 -0400

    Expose `summarize_combo` as a private function so phenoptrReports can use it.

--- R/average_counts_rtree.R
+++ R/average_counts_rtree.R
@@ -32,52 +32,52 @@ count_within_many_impl_rtree = function(csd, name, combos, radii,
                        count_within_detail(d_cat, to_phenotypes, radii))
     })
 
-
-  # Compute summary stats for a single dataset, a single category and
-  # (from, to) pair, and all radii
-  summarize_combo = function(d, category, from, to) {
-    if (!is.na(category))
-      d_cat = d %>% dplyr::filter(`Tissue Category`==category)
-    else {
-      d_cat = d
-      category = 'all'
-    }
-
-    to_count = sum(phenoptr::select_rows(d_cat, phenotype_rules[[to]]))
-
-    # The rows of interest
-    rows = d_cat %>%
-      dplyr::filter(phenoptr::select_rows(., phenotype_rules[[from]]))
-
-    # Now we can summarize per radius
-    if (nrow(rows) > 0) {
-      purrr::map_dfr(radii, function(radius) {
-        count_col = count_col_name(to, radius)
-        counts = rows[[count_col]] # Single column of interest
-
-        tibble::tibble(category=category,
-                       from=from, to=to, radius=radius,
-                       from_count=nrow(rows), to_count=to_count,
-                       from_with = sum(counts>0, na.rm=TRUE),
-                       within_mean = mean(counts, na.rm=TRUE))
-    })
-    } else {
-      tibble::tibble(
-        category=category,
-        from=from, to=to, radius = radii,
-        from_count = 0L,
-        to_count = to_count,
-        from_with = 0L,
-        within_mean = NA
-      )
-
-    }
-  }
-
   # Now we can do the work, computing summary stats for all combos and radii
   purrr::map_dfr(combos, function(combo) {
-    summarize_combo(counts, combo$category, combo$pair[[1]], combo$pair[[2]])
+    summarize_combo(counts, combo$category, phenotype_rules,
+                    combo$pair[[1]], combo$pair[[2]], radii)
   })
+}
+
+# Compute summary stats for a single dataset, a single category and
+# (from, to) pair, and all radii
+summarize_combo = function(d, category, phenotype_rules, from, to, radii) {
+  if (!is.na(category))
+    d_cat = d %>% dplyr::filter(`Tissue Category`==category)
+  else {
+    d_cat = d
+    category = 'all'
+  }
+
+  to_count = sum(phenoptr::select_rows(d_cat, phenotype_rules[[to]]))
+
+  # The rows of interest
+  rows = d_cat %>%
+    dplyr::filter(phenoptr::select_rows(., phenotype_rules[[from]]))
+
+  # Now we can summarize per radius
+  if (nrow(rows) > 0) {
+    purrr::map_dfr(radii, function(radius) {
+      count_col = count_col_name(to, radius)
+      counts = rows[[count_col]] # Single column of interest
+
+      tibble::tibble(category=category,
+                     from=from, to=to, radius=radius,
+                     from_count=nrow(rows), to_count=to_count,
+                     from_with = sum(counts>0, na.rm=TRUE),
+                     within_mean = mean(counts, na.rm=TRUE))
+    })
+  } else {
+    tibble::tibble(
+      category=category,
+      from=from, to=to, radius = radii,
+      from_count = 0L,
+      to_count = to_count,
+      from_with = 0L,
+      within_mean = NA
+    )
+
+  }
 }
 
 
